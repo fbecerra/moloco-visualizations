d6b449d3fed414cadf20884ce39750ba78ad564b
Fernando Becerra <becerrafernando@gmail.com>
2024-10-09 12:59:30 -0300

    add bars to right panel

--- js/viz6.js
+++ js/viz6.js
@@ -1,5 +1,9 @@
 function drawViz6() {
 
+    const gray = '#ECEDEE';
+    const blue = '#0280FB';
+    const darkerGray = "#D9D9D9";
+
     Promise.all([
         // d3.json('./data/countries-50m.json'),
         d3.json('./data/world-110m-2024.json'),
@@ -27,10 +31,85 @@ function drawViz6() {
         const leftPanel = gridWrapper.append("div")
             .attr("class", "left-panel");
 
-        gridWrapper.append("div")
+        const rightPanel = gridWrapper.append("div")
             .attr("class", "right-panel")
-            .style("background-color", "#d2d2d2")
-            .html("Countries");
+            .attr("id", "right-panel")
+            .style("background-color", gray)
+            .style("padding", "8px 10px")
+
+        // RIGHT PANEL
+
+        addBoldText('#right-panel', "Countries");
+
+        const countriesString = rightPanel.append("div");
+        const budgetBar = rightPanel.append("div")
+            .attr("id", "budget")
+            .style("height", '60px');
+        const revenueBar = rightPanel.append("div")
+            .attr("id", "revenue")
+            .style("height", '60px');
+
+        rightPanel.append("div")
+            .attr("id", "arppu")
+            .style("height", '60px');
+
+        function addBar(divId, legendText) {
+            const bar = d3.select("#" + divId).append('div')
+                .attr('class', 'bar')
+                .style("position", "relative")
+                .style("width", '100%')
+                .style("margin", 'auto')
+                .style("height", divId === 'arppu' ? '17px' : '11px')
+
+            bar.append("div")
+                .style("width", "100%")
+                .style("background-color", darkerGray)
+                .style("height", '11px')
+                .style("position", 'absolute')
+                .style("top", divId === 'arppu' ? 3 : 0)
+                .style("left", 0)
+
+            bar.append("div")
+                .attr("id", divId + '-bar')
+                .style("width", "20%")
+                .style("background-color", blue)
+                .style("height", divId === 'arppu' ? '17px' : '11px' )
+                .style("position", 'absolute')
+                .style("top", 0)
+                .style("left", 0);
+
+            const legend = d3.select("#" + divId).append("div")
+                .attr("class", 'legend')
+                .style('position', 'relative')
+                .style("width", '100%')
+                .style("margin", 'auto');
+
+            legend.append("div")
+                .attr("id", divId + '-legend')
+                .style("width", "40%")
+                .style("position", 'absolute')
+                .style("font-size", "32px")
+                .style("color", blue)
+                .style("top", 0)
+                .style("left", 0)
+                .html("52%")
+
+            legend.append("div")
+                .style('text-align', 'right')
+                .style("width", "60%")
+                .style("position", 'absolute')
+                .style("top", 0)
+                .style("right", 0)
+                .html(legendText)
+        }
+
+        addBar("budget", "paid UA budget");
+        addBar("revenue", "gaming revenue");
+        addBar("arppu", "ARPPU");
+        
+        
+
+        // LEFT PANEL
 
         const buttonsWrapper = leftPanel.append("div")
             .attr("class", "button-wrapper")
@@ -43,7 +122,7 @@ function drawViz6() {
             .data(groups)
             .join("div")
                 .attr("class", "country-button")
-                .style("background-color", d => d === selectedGroup ? "#0280FB" : "#ECEDEE")
+                .style("background-color", d => d === selectedGroup ? blue : gray)
                 .style("color", d => d === selectedGroup ? "#FFFFFF" : "#000000")
                 .style("cursor", "pointer")
                 .html(d => d)
@@ -51,7 +130,7 @@ function drawViz6() {
                     if (selectedGroup !== d) {
                         selectedGroup = d;
                         d3.selectAll(".country-button")
-                            .style("background-color", d => d === selectedGroup ? "#0280FB" : "#ECEDEE")
+                            .style("background-color", d => d === selectedGroup ? blue : gray)
                             .style("color", d => d === selectedGroup ? "#FFFFFF" : "#000000")
                         updatePlot();
                     }
@@ -121,8 +200,29 @@ function drawViz6() {
         
         const g = svg.append("g");
 
+        let countriesLabel;
+        let paidBudgetLabel;
+        let revenueLabel;
+        let minARPPU, maxARPPU;
+        let minMaxLabel;
+
+        function updateRightPanel() {
+            countriesString.html(countriesLabel);
+            d3.select("#budget-bar").style("width", paidBudgetLabel);
+            d3.select("#budget-legend").html(paidBudgetLabel);
+            d3.select("#revenue-bar").style("width", revenueLabel);
+            d3.select("#revenue-legend").html(revenueLabel);
+        }
+
         function updatePlot() {
             const groupCountries = values.filter(value => (value.Tier === selectedGroup) & (value.Genre === selectedGenre));
+            countriesLabel = groupCountries.map(d => d['Market full name']).join(', ');
+            paidBudgetLabel = (groupCountries.reduce((a,b) => a + b['UA Spend'], 0) * 100).toFixed(0) + '%';
+            revenueLabel = (groupCountries.reduce((a,b) => a + b['Revenue'], 0) * 100).toFixed(0) + '%';
+            [minARPPU, maxARPPU] = d3.extent(groupCountries, d => d.ARPPU);
+            // minMaxLabel = `$${minARPPU.toFixed(1)}-$${maxARPPU.toFixed(1)}`
+
+            updateRightPanel();
 
             const abbvCountries = groupCountries.map(d => d.Market);
 
@@ -130,7 +230,7 @@ function drawViz6() {
                 .data(countries.features)
                 .join("path")
                     // .attr("fill", d => color(valuemap.get(d.properties.name)))
-                    .attr("fill", d => abbvCountries.indexOf(d.properties.a3) > 0 ? "#0280FB" : "#ECEDEE")
+                    .attr("fill", d => abbvCountries.indexOf(d.properties.a3) > 0 ? blue : gray)
                     .attr("d", path)
         
         
