c1142abddbbfd8e2844302b9c369cd1cde041923
evelinapar <121967120+evelinapar@users.noreply.github.com>
2024-10-14 10:11:33 +0200

    Removed <br>

--- js/viz4.js
+++ js/viz4.js
@@ -173,7 +173,7 @@ function drawViz4() {
             ylabel = 'Market full name',
             title = "Marketing spend and total in-app purchase revenue is aligned in most countries",
             subtitle = "Mobile gaming (IAP) user acquisition spend compared to mobile gaming (IAP) D7 revenue (Moloco customers)",
-            sources = "Source: Moloco estimates of mobile gaming (IAP) user acquisition spend (2025) & Moloco advertiser total gaming IAP revenue (organic and paid) by market (Sep 2023 to August 2024). Spend in mainland China is excluded from </br>this analysis, but spend by mobile gaming apps based in China in non-domestic markets is included."
+            sources = "Source: Moloco estimates of mobile gaming (IAP) user acquisition spend (2025) & Moloco advertiser total gaming IAP revenue (organic and paid) by market (Sep 2023 to August 2024). Spend in mainland China is excluded from this analysis, but spend by mobile gaming apps based in China in non-domestic markets is included."
         )
         
     })
