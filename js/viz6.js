0342b9b165bbd821d372b4109e0db636ddae529b
evelinapar <121967120+evelinapar@users.noreply.github.com>
2024-10-14 18:35:17 +0200

    Update viz6.js: trying to fix title and subtitle

--- js/viz6.js
+++ js/viz6.js
@@ -77,12 +77,12 @@ function drawViz6() {
 
         d3.select("#geo-viz6 .title")
             .style("max-width", "700px")
-            //.style("text-align", "left") // Ensure left alignment
-            .style("margin", "0")
-            //.style("margin", "auto");
+            .style("text-align", "left") 
+            .style("margin", "0 auto")
 
         d3.select("#geo-viz6 .subtitle")
             .style("max-width", "700px")
+            .style("text-align", "left") 
             .style("margin", "8px auto 24px auto");
 
         // End of NEW to test
