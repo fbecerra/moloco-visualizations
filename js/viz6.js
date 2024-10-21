815258c11011cf94217bbc2cb85f57fc9fcb7988
Fernando Becerra <becerrafernando@gmail.com>
2024-10-21 13:33:30 -0300

    fix tooltip bug for ua spend

--- js/viz6.js
+++ js/viz6.js
@@ -391,7 +391,7 @@ function drawViz6() {
                                             </td>
                                         </tr>
                                         <tr>
-                                            <td style="padding-right: ${paddingRows}px;font-family: 'Spacegrotesk';font-size: 14px;font-weight: 500;padding-bottom: 6px">${(thisCountry['UA spend'] * 100).toFixed(1)}%
+                                            <td style="padding-right: ${paddingRows}px;font-family: 'Spacegrotesk';font-size: 14px;font-weight: 500;padding-bottom: 6px">${(thisCountry['UA Spend'] * 100).toFixed(1)}%
                                             </td>
                                             <td style="padding-right: ${paddingRows}px;font-family: 'Spacegrotesk';font-size: 14px;font-weight: 500;padding-bottom: 6px">${(thisCountry.Revenue * 100).toFixed(1)}%
                                             </td>
